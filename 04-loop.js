54dbaf9add208729f0c9d7d5f0193c940a3af82d
ch <mkmsacc@outlook.com>
2022-01-27 16:39:06 +0700

    Add loop.js

---
+++ 04-loop.js
@@ -0,0 +1,14 @@
+let i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+i = 0;
+do {
+    i++;
+    console.log(i);
+} while (i < 10);
+for (let j = 0; j < 1000; j++) {
+    console.log(j);
+}
+
