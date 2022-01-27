94715828f51c2447cd8745013f1850c7c5f461e6
ch <mkmsacc@outlook.com>
2022-01-27 16:38:50 +0700

    Add var.js

---
+++ 02-var.js
@@ -0,0 +1,8 @@
+const number = 1;
+console.log(typeof number);
+const pi = Math.PI;
+console.log(pi);
+const s = '2';
+console.log(Number(s) + number);
+const float = 2.22;
+console.log(Number(float) + number);
